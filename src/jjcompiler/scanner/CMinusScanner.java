3c7025d61cf381133d54e960553c4b4c84339cf1
Jake Allinson <jakeallinson03@gmail.com>
2021-02-11 10:46:43 -0500

    implement stuff from book and slides

--- src/jjcompiler/scanner/CMinusScanner.java
+++ src/jjcompiler/scanner/CMinusScanner.java
@@ -1,16 +1,37 @@
 package jjcompiler.scanner;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CMinusScanner implements Scanner {
 
     private BufferedReader inFile;
     private Token nextToken;
 
+    private enum FAState {
+        START,
+        INCOMMENT,
+        INNUM,
+        INID,
+        INASSIGN,
+        DONE,
+        ERROR
+    }
+
     public CMinusScanner (BufferedReader file) {
         inFile = file;
         nextToken = scanToken();
     }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader("resources/example1.cm"));
+        CMinusScanner scanner = new CMinusScanner(reader);
+
+        System.out.println(scanner.getNextToken().getType());
+    }
+
     public Token getNextToken () {
         Token returnToken = nextToken;
         if (nextToken.getType() != Token.TokenType.EOF_TOKEN)
@@ -20,8 +41,27 @@ public class CMinusScanner implements Scanner {
     public Token viewNextToken () {
         return nextToken;
     }
-    public Token scanToken () {
-        // IMPLEMENT THIS FUNCT
+    // PROJ 1: IMPLEMENT THIS FUNCT
+    private Token scanToken () {
+        try {
+            FAState state = FAState.START;
+            StringBuilder content = new StringBuilder();
+
+            int value;
+            while (state != FAState.START) {
+
+            }
+            while ((value = inFile.read()) != -1) {
+                char nextChar = (char) value;
+
+                content.append(nextChar);
+            }
+
+            // System.out.println(content.toString());
+        } catch (IOException e) {
+
+        }
+
         return new Token(Token.TokenType.EOF_TOKEN);
     }
 }
