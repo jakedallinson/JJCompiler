43a606f50039a92c61c882b1ec981dc3fcf4a67b
paperboycreates <paperboycreates@gmail.com>
2021-03-20 19:51:46 -0400

    squash! set-up parser class with exception class

--- src/jjcompiler/parser/Parser.java
+++ src/jjcompiler/parser/Parser.java
@@ -1,11 +1,12 @@
 package jjcompiler.parser;
 
-import jjcompiler.parser.AST.*;
-import jjcompiler.scanner.*;
+import jjcompiler.parser.AST.Program;
+import jjcompiler.scanner.Token;
+
 import java.io.IOException;
 
 public interface Parser {
     public Program parse() throws IOException;
-    public void advanceToken() throws IOException;
-    public void matchToken(Token token) throws CMinusParserException;
+    public Token advanceToken() throws IOException;
+    public void matchToken(Token.TokenType expected) throws CMinusParserException, IOException;
 }
