5c672b5c369496f9a1b380d7d0cc07da16517e0a
Jake Allinson <jakeallinson03@gmail.com>
2021-04-22 17:23:37 -0400

    call expr implementation

---
+++ output/ex7.s
@@ -0,0 +1,10 @@
+.text
+	.align 4
+.globl  main
+main:
+main_bb2:
+main_bb3:
+	movl	$0, %EDI
+	call	expo
+main_bb1:
+	ret
--- src/jjcompiler/compiler/CMinusCompiler.java
+++ src/jjcompiler/compiler/CMinusCompiler.java
@@ -126,7 +126,7 @@ public class CMinusCompiler implements Compiler {
     }
 
     public static void main(String[] args) {
-        String filePrefix = "ex6";
+        String filePrefix = "ex7";
         CMinusCompiler myCompiler = new CMinusCompiler();
         myCompiler.setGenX64Code(true);
         myCompiler.compile(filePrefix);
